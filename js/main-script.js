eec9a54ea53f251a18f37cb858ec65ba43369401
Shahidul <ishahin56@gmail.com>
2022-09-03 13:21:30 +0600

    Completed responsive sidebar dropdown issue

--- js/main-script.js
+++ js/main-script.js
@@ -1,5 +1,33 @@
 "use strict"
-const sidebarNav = document.querySelector(".sidebar nav")
-const navLink = document.querySelectorAll('.sidebar nav .nav-link');
+const sidebar = document.querySelector(".sidebar")
+const navLink = document.querySelectorAll('.sidebar .nav .btn-toggle');
 
-console.log(navLink);
+const smallSidebarScreen = 1200;
+
+window.addEventListener("resize", function () {
+   sidebarExpandOnSmallerScreen();
+})
+
+window.addEventListener("load", function () {
+   sidebarExpandOnSmallerScreen();
+})
+
+function sidebarExpandOnSmallerScreen() {
+   const wndWidth = window.innerWidth;
+   if (wndWidth < smallSidebarScreen && wndWidth > 767) {
+      if (navLink.length !== 0) {
+         navLink.forEach(function (btnToggle) {
+            btnToggle.addEventListener("click", function () {
+               const expanded = btnToggle.getAttribute("aria-expanded");
+               if (expanded === "true") {
+                  sidebar.classList.add('show');
+               } else {
+                  sidebar.classList.remove('show');
+               }
+               // if () {
+               // }
+            })
+         })
+      }
+   }
+}
